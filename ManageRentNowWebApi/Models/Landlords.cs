3fe49d8c5faf128b57265cafb65c265bfc5ebe9a
William Herr <herrwilliam@outlook.com>
2024-06-14 22:58:32 -0400

    Add project files.

---
+++ ManageRentNowWebApi/Models/Landlords.cs
@@ -0,0 +1,12 @@
+﻿namespace ManageRentNow.Api.Models
+{
+    public class Landlords
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+
+    }
+}
---
+++ ManageRentNowWebApi/Models/Properties.cs
@@ -0,0 +1,9 @@
+﻿namespace ManageRentNow.Api.Models
+{
+    public class Properties
+    {
+        public Landlords Landlord { get; set; }
+        public List<Tenant> Tenants { get; set; }
+
+    }
+}
